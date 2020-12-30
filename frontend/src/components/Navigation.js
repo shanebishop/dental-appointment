9a306d4c843e6cc1ff57194723c980995c05155d
Shane Bishop <shane.bishop@outlook.com>
2020-12-30 10:26:46 -0500

    Cleanup Navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -6,38 +6,15 @@ import { toggleSidebar } from "../redux/actions/sidebarActions";
 import {
   Button,
   Navbar,
-  NavbarBrand,
   Collapse,
   Nav,
 } from "reactstrap";
-
-import { Home } from "react-feather";
 
 class Navigation extends React.Component {
   loggedIn() {
     const state = store.getState();
     return state.auth && state.auth.loggedIn;
   }
-
-  // render() {
-  //   const isLoggedIn = this.loggedIn();
-  //
-  //   return (
-  //     <Navbar dark expand className="navbar-landing">
-  //       <NavbarBrand href="/">
-  //         <Home title="Dr. Phil Ing Dental Clinic" />
-  //         Dr. Phil Ing Dental Clinic
-  //       </NavbarBrand>
-  //       <Button
-  //         href={isLoggedIn ? '/auth/sign-out' : '/auth/sign-in'}
-  //         color="primary"
-  //         className="ml-auto ml-2"
-  //       >
-  //         {isLoggedIn ? 'Logout' : 'Login'}
-  //       </Button>
-  //     </Navbar>
-  //   );
-  // }
 
   render() {
     return (
