5cbeb0c1a7cedcc67d108fa1246fab66d6810789
Shane Bishop <shane.bishop@outlook.com>
2021-01-19 17:08:04 -0500

    Both table of contents and images are displaying now

--- scripts/compile_report.sh
+++ scripts/compile_report.sh
@@ -4,18 +4,15 @@
 [ -f report.mom ] || { echo "report.mom not found in $(pwd)."; exit 1; }
 
 # Compile to PDF
-# pdfmom -Tps report.mom > report.pdf || exit "$?" # This is the mom version
-# groff -me -Tps report.me | ps2pdf - report.pdf || exit "$?" # This is the me version
-
-# Newer mom version
 # First, create PS
-groff -Tps -mom report.mom > report.ps || exit "$?"
+# groff -Tps -mom report.mom > report.ps || exit "$?" # Cannot use -Tps if we want automatic relocation
+pdfmom report.mom > report.pdf || exit "$?"
 # Then, move the table of contents where it belongs, and generate PDF
 # What the -p argument is saying:
 # 1 - the first page of the document
 # _1 - the last page of the document
 # 2-_2 - the second page of the document to the second last page of the document
-psselect -p 1,_1,2-_2 report.ps | ps2pdf - report.pdf || exit "$?"
+# psselect -p 1,_1,2-_2 report.ps | ps2pdf - report.pdf || exit "$?"
 
 # If evince is not running, start evince
 command -v evince > /dev/null 2>&1 && {
