21b53456af00b5d299022189545a53602ea713a0
Shane Bishop <shane.bishop@outlook.com>
2021-01-19 16:24:46 -0500

    Initial attempt at groff report

---
+++ report/README.md
@@ -0,0 +1,5 @@
+# Report
+
+This directory contains the groff source for my Honours Project report.
+
+To generate the report as `report.pdf`, run `./compile`.
---
+++ scripts/compile_report.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+[ -f report.mom ] || cd report || { echo 'cd failed.'; exit 1; }
+[ -f report.mom ] || { echo "report.mom not found in $(pwd)."; exit 1; }
+
+# Compile to PDF
+# pdfmom -Tps report.mom > report.pdf || exit "$?" # This is the mom version
+# groff -me -Tps report.me | ps2pdf - report.pdf || exit "$?" # This is the me version
+
+# Newer mom version
+# First, create PS
+groff -Tps -mom report.mom > report.ps || exit "$?"
+# Then, move the table of contents where it belongs, and generate PDF
+# What the -p argument is saying:
+# 1 - the first page of the document
+# _1 - the last page of the document
+# 2-_2 - the second page of the document to the second last page of the document
+psselect -p 1,_1,2-_2 report.ps | ps2pdf - report.pdf || exit "$?"
+
+# If evince is not running, start evince
+command -v evince > /dev/null 2>&1 && {
+    pgrep evince > /dev/null 2>&1
+    [ "$?" -eq 1 ] && nohup evince report.pdf > /dev/null 2>&1 &
+}
